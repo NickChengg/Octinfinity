2eee84fe0d48f99a35f367d3088681c32d201cf4
NickChengg <44110748+NickChengg@users.noreply.github.com>
2018-11-18 01:17:15 +0800

    Update main.c

--- src/user/main.c
+++ src/user/main.c
@@ -24,21 +24,242 @@
 #include "pwm.h"
 #include "adc.h"
 
-u16 fullSpeed =100;
-int16_t OC1 = 0, OC2 = 0;//
-u32 move_tick = 0; //count 1s
-u8 debounce1 = 0, debounce2 = 0, debounce3 = 0;
-u8 moveDirection1 = 1, moveDirection2 = 1;
-u8 select_motor = 1;
+//def
+enum {
+STILL = 0, //stay still, indicate waiting for next movement
+FORWARD = 1, //move forward
+LEFTTURN = 2, // 90 degree left turn
+RIGHTTURN = 3, // 90 degree right turn
+ABOUTTURN = 4, // 180 degree right turn
+ACTION = 5	//non-movement: picking or throwing
+} MOVEMENT;
+
+
+enum {
+INIT = 0, //wait to start
+TO_LZ = 1, //move forward
+PICK_RACK = 2, // action
+	
+TO_TZ_1TURN = 3, // about turn
+TO_TZ_2MOVE = 4, // forward
+TO_TZ_3TURN = 5, // leftturn
+TO_TZ_4MOVE = 6, // forward
+TO_TZ_5TURN = 7, // right turn
+TO_TZ_6MOVE = 8, // forward
+TO_TZ_7TURN = 9, // about turn (~150 degree to right)
+	
+THROWING = 10, // action
+LEAVE_TZ = 11, // right turn (~30 degree)
+
+STAY = 15,
+} PROGRESS;
+
+//init
+u8 movement= STILL;
+u8 progress = INIT;
+u32 escape_ticks = 0; //time of start turning
+u8 move_count = 0; //no. of grip+turn to go
+
+//testing value
+u16 const motor1_fullSpeed =100, motor2_fullSpeed =100; // test value, range ~100, <=100
+int32_t const motor_turnTO_OC = 0, motor_turnAWAY_OC = 0;// test vlaue, turn TO the direction(TO < AWAY). range: -100 to 100
+u32 const turn_90_ticks = 100, turn_180_ticks = 200; // test value, for ticks difference of turning 90 or 180 degree
+u32 const turn_30_ticks = 20; //test value, for ticks difference of turning 30 degree(to face to house)
+
+void compensate_cal(int L_reading, int R_reading) { //time difference from left&right reading from line sensors
+	//edwin: change the speed of each motor
+	
+	 // 
+}
+
+void motor_move(int motor_left_OC, int motor_right_OC) {
+	u8 moveDirection_left = motor_left_OC > 0 ? 1 : 0; //
+	u8 moveDirection_right = motor_right_OC > 0 ? 1 : 0; //
+	motor_control(MOTOR1, ABS((int)motor_left_OC), moveDirection_left);
+	motor_control(MOTOR2, ABS((int)motor_right_OC), moveDirection_right);
+}
+void motor_action(u32 this_ticks) {
+	switch (progress) {
+		case INIT: {
+			if (1) {	//start condition
+				//init next stage
+				progress = TO_LZ;
+				movement = FORWARD;
+				move_count = 5; //sample value, change later
+			}
+			break;
+		}
+		case TO_LZ: {
+			if (movement == STILL) { //finish movement, arrived LZ
+				//init next stage
+				progress = PICK_RACK;
+				movement = ACTION;
+			}
+			break;
+		}
+		case PICK_RACK: {
+			// some code below
+			
+			
+			if (1) {// picked, can be moved to other function
+				movement = STILL; //indicate end of action
+			}
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_1TURN; //about turn
+				movement = ABOUTTURN;
+			}
+			break;
+		}
+		case TO_TZ_1TURN: { //about turn
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_2MOVE; //forward
+				movement = FORWARD;
+				move_count = 5; //sample value, change later
+			}
+			break;
+		}
+		case TO_TZ_2MOVE: { //forward
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_3TURN; //left turn
+				movement = LEFTTURN;
+			}
+			break;
+		}
+		case TO_TZ_3TURN: { //left turn
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_4MOVE; //forward
+				movement = FORWARD;
+				move_count = 5; //sample value, change later
+			}
+			break;
+		}
+		case TO_TZ_4MOVE: { //forward
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_5TURN; //right turn
+				movement = RIGHTTURN;
+			}
+			break;
+		}
+		case TO_TZ_5TURN: { //right turn
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_6MOVE; //forward
+				movement = FORWARD;
+				move_count = 5; //sample value, change later
+			}
+			break;
+		}
+		case TO_TZ_6MOVE: { //forward
+			if (movement == STILL) { 
+				//init next stage
+				progress = TO_TZ_7TURN; //about turn
+				movement = ABOUTTURN; //lesser than an about turn
+				escape_ticks = this_ticks - turn_30_ticks; //sample value, change later
+			}
+			break;
+		}
+		case TO_TZ_7TURN: { //about turn (~150 degree to right)
+			if (movement == STILL) { 
+				//init next stage
+				progress = THROWING; //action
+				movement = ACTION;
+			}
+			break;
+		}
+		case THROWING: {
+			// more code below, controlling the moving component
+			
+			
+			if (movement == STILL) { 
+				//init next stage
+				progress = STAY; //stay still, end of tasks
+				movement = STILL; 
+			}
+			break;
+		}
+
+	}
+	
+	switch (movement) {
+		case STILL:
+			break;
+		case FORWARD: {
+			if (move_count > 0) {
+				motor_move(motor1_fullSpeed, motor2_fullSpeed);
+				/*edwin: line sensor
+				
+				if (cross line)*/ {
+					move_count--;
+					escape_ticks = this_ticks;//need?
+				}
+			}
+			else movement = STILL;
+			break;
+		}
+		case LEFTTURN: {
+			if (escape_ticks == 0) { //init escape ticks
+				escape_ticks = this_ticks;
+			}
+			else { //start turning
+				if ((this_ticks - escape_ticks) <= turn_90_ticks) { //turn
+					motor_move(motor_turnTO_OC, motor_turnAWAY_OC);
+				}
+				else { //stop turning
+					escape_ticks = 0;
+					movement = STILL;
+				}
+			}
+			break;
+		}
+		case RIGHTTURN: {
+			if (escape_ticks == 0) { //init the escape ticks
+				escape_ticks = this_ticks;
+			}
+			else { //start turning
+				if ((this_ticks - escape_ticks) <= turn_90_ticks) { //turn
+					motor_move(motor_turnAWAY_OC, motor_turnTO_OC);
+				}
+				else { //stop turning
+					escape_ticks = 0;
+					movement = STILL;
+				}
+			}
+			break;
+		}
+		case ABOUTTURN: {
+			if (escape_ticks == 0) { //init escape ticks
+				escape_ticks = this_ticks;
+			}
+			else { //start turning
+				if ((this_ticks - escape_ticks) <= turn_180_ticks) { //turn
+					motor_move(motor_turnAWAY_OC, motor_turnTO_OC);
+				}
+				else { //stop turning
+					escape_ticks = 0;
+					movement = STILL;
+				}
+			}
+			break;
+		}
+	}
+}
 
 int main() {
 	// Initialize Everything Here
 	rcc_init();
 	ticks_init();
-	buttons_init();
-	tft_init(PIN_ON_TOP, WHITE, BLACK, RED, YELLOW); 
-	motor_init(MOTOR1, 144, fullSpeed, OC1, moveDirection1); //at rest
-	motor_init(MOTOR2, 144, fullSpeed, OC2, moveDirection2); //at rest
+	oled_init();
+	motor_init(MOTOR1, 144, 100, 0, 1); //at rest
+	motor_init(MOTOR2, 144, 100, 0, 1); //at rest
+	tft_init(PIN_ON_TOP, WHITE, BLACK, RED, YELLOW); //debug
+	uart_init(COM1,115200); //debug
+	
+	
 	
 	while (1) {
 		static u32 this_ticks = 0;
@@ -46,65 +267,16 @@ int main() {
 		this_ticks = get_ticks();
 
 		static u32 last_led_ticks=0;
-		if ((this_ticks - last_led_ticks) >= 10) {
+		if ((this_ticks - last_led_ticks) >= 25) {
 			last_led_ticks = this_ticks;
-			//Code in here will run every xx ms
-			
-			if (!button_pressed(BUTTON1) && debounce1) {	//button pressed
-				debounce1 = 0;
-			}
-			if (button_pressed(BUTTON1) && !debounce1) {
-				debounce1 = 1;
-				if (select_motor == 1)
-					OC1 += fullSpeed / 20;
-				else OC2 += fullSpeed / 20;
-				
-			}
-			
-			if (!button_pressed(BUTTON2) && debounce2) {
-				debounce2 = 0;
-			}
-			if (button_pressed(BUTTON2) && !debounce2) {
-				debounce2 = 1;
-				if (select_motor == 1)
-					OC1 -= fullSpeed / 20;
-				else OC2 -= fullSpeed / 20;
-				
-			}
-			
-			if (!button_pressed(BUTTON3) && debounce3) {
-				debounce3 = 0;
-			}
-			if (button_pressed(BUTTON3) && !debounce3) {
-				debounce3 = 1;
-				if (select_motor == 1) {
-					select_motor = 2;
-				}
-				else select_motor = 1;
-			}
-
-			OC1 = OC1>100? 100: OC1;
-			OC1 = OC1<-100? -100: OC1;
-			OC2 = OC2>100? 100: OC2;
-			OC2 = OC2<-100? -100: OC2;
-			
-			if (OC1>=0) {
-				moveDirection1 = 1;
-			}
-			else {
-				moveDirection1 = 0;
-			}
-			if (OC2>=0) {
-				moveDirection2 = 1;
-			}
-			else {
-				moveDirection2 = 0;
-			}
+			//Code in here will run every 25ms
 			tft_clear();
-			tft_prints(0,0,"OC1:%d\nOC2:%d\n\nmotor sel:%d", OC1, OC2, select_motor);
+			
+			motor_action(this_ticks);
+			tft_prints(0, 0, "movement: %d\nprogress: %d", movement,progress);
 			tft_update();
-			motor_control(MOTOR1, ABS((int)OC1), moveDirection1);
-			motor_control(MOTOR2, ABS((int)OC2), moveDirection2);
+			uart_tx_str(COM1, "movement: %d\nprogress: %d", movement,progress);
+			
 		}
 	}
 }
