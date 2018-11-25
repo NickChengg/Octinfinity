6ca1d94ae0a19c1e486251816306bf2e245a43df
EdwinGame <44203780+EdwinGame@users.noreply.github.com>
2018-11-25 17:05:03 +0800

    Add files via upload

--- testing_library/sonic-2nd mechanism/sonic.c
+++ testing_library/sonic-2nd mechanism/sonic.c
@@ -1,5 +1,6 @@
 #include "sonic.h"
 #include "gpio.h"	
+#include "lcd_main.h"
 
 #define US_TO_SYS 72.4637681159    						//1000.0/13.8;
 #define US_TO_MM  0.17												//1e-6 * 340.0 / 2 * 1e3
@@ -8,12 +9,13 @@ GPIOPin trig_pin, echo_pin;
 static u8 sonic_received = 0;
 
 uint32_t sonic_dur, sonic_mm;
+u32 rise = 0, fall = 0;
 
 void sonic_init(const GPIOPin* trig, const GPIOPin* echo){
 	trig_pin = *trig;
 	echo_pin = *echo;
-	gpio_init(&trig_pin, GPIO_Mode_Out_PP);	 	//not sure about the init mode is correct
-	gpio_init(&echo_pin, GPIO_Mode_IPU);				//not sure about the init mode is correct
+	gpio_init(GPIO7, GPIO_Mode_Out_PP);	 	//not sure about the init mode is correct
+	gpio_init(GPIO8, GPIO_Mode_IPU);				//not sure about the init mode is correct
 }
 
 //regularly send pulse with fixed length
@@ -24,9 +26,9 @@ void sonic_cycle(){
 	if(!sonic_sent){
 		//Create trigger pulse of 10us (stay here)
 		triggered_sysTicks = SysTick->VAL;
-		gpio_write(&trig_pin,1);
+		gpio_write(GPIO7,1);
 		while(SysTick->VAL - triggered_sysTicks < (10.0*US_TO_SYS));
-		gpio_write(&trig_pin,0);
+		gpio_write(GPIO7,0);
 		sonic_sent = 1;
 	} else {
 		//Wait for 60ms (pass until condition satisfied)
@@ -46,11 +48,13 @@ void sonic_echo_handler(){
 	//placed in IRQHandler of EXTI_Trigger_Rising_Falling
 	static u32 received_sysTicks;
 	
-	if(gpio_read(&echo_pin)){		//rising
+	if(gpio_read(GPIO8)){		//rising
 		received_sysTicks = SysTick->VAL;
+		tft_prints(0,3,"rise:%d",(int)rise++);
 	} else {										//falling
+		tft_prints(0,4,"fall:%d",(int)fall++);
 		sonic_dur = SysTick->VAL - received_sysTicks;
-		sonic_mm = (double)sonic_dur * US_TO_MM * 10.0;
+		sonic_mm = (double)sonic_dur / US_TO_SYS * US_TO_MM * 10.0;
 		sonic_received = 1;
 	}	
 }
--- testing_library/sonic-2nd mechanism/sonic.h
+++ testing_library/sonic-2nd mechanism/sonic.h
@@ -1,32 +1,33 @@
-#ifndef __SONIC_H
-#define __SONIC_H
+	#ifndef __SONIC_H
+	#define __SONIC_H
 
-#include "gpio.h"	 
+	#include "gpio.h"	 
 
-extern uint32_t sonic_dur, sonic_mm;
+	extern uint32_t sonic_dur, sonic_mm;
+	extern u32 rise, fall;
 
-/**
- * @brief      Initialize ultrasonic sensors
- *
- * @param      trig      output pin sending sonic
- * @param      echo		   input pin receiving sonic) 
- *
- * @note			 pin names swapped compared to previous version
- */
-void sonic_init(const GPIOPin* trig, const GPIOPin* echo);
+	/**
+	 * @brief      Initialize ultrasonic sensors
+	 *
+	 * @param      trig      output pin sending sonic
+	 * @param      echo		   input pin receiving sonic) 
+	 *
+	 * @note			 pin names swapped compared to previous version
+	 */
+	void sonic_init(const GPIOPin* trig, const GPIOPin* echo);
 
-/**
- * @brief      Regularly send pulse with fixed length
- *
- * @note			 execute once every shortest cycle (precision: sysTicks)
- */
-void sonic_cycle(void);
+	/**
+	 * @brief      Regularly send pulse with fixed length
+	 *
+	 * @note			 execute once every shortest cycle (precision: sysTicks)
+	 */
+	void sonic_cycle(void);
 
-/**
- * @brief      Calculate distance based on 2nd mechanism (duration of high-level pulse)
- *
- * @note			 placed in IRQHandler (mode: EXTI_Trigger_Rising_Falling)
- */
-void sonic_echo_handler(void);
+	/**
+	 * @brief      Calculate distance based on 2nd mechanism (duration of high-level pulse)
+	 *
+	 * @note			 placed in IRQHandler (mode: EXTI_Trigger_Rising_Falling)
+	 */
+	void sonic_echo_handler(void);
 
-#endif
+	#endif
