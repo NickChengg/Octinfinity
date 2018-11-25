ca9cd394938a3b675d61213be280dbe935e591f3
EdwinGame <44203780+EdwinGame@users.noreply.github.com>
2018-11-25 17:05:44 +0800

    Update main.c

--- testing_library/sonic-2nd mechanism/main.c
+++ testing_library/sonic-2nd mechanism/main.c
@@ -31,6 +31,7 @@ int main() {
 	rcc_init();
 	ticks_init();
 	leds_init();
+	uart_init(COM1,115200);
 	tft_init(0, BLUE, WHITE, GREEN, DARK_RED);
 	sonic_init(TRIG_PIN, ECHO_PIN);
 	gpio_exti_init(ECHO_PIN, EXTI_Trigger_Rising_Falling);
@@ -47,11 +48,14 @@ int main() {
 		this_ticks = get_ticks();
 		
 		//per 25 ticks
-		if(this_ticks - last_ticks_25 > 25){
+		if(this_ticks - last_ticks_25 > 100){
 			last_ticks_25 = this_ticks;
-			tft_clear();
-			tft_prints(0,0,"Dur:%d\nmm:%f\nCnt:%d",sonic_dur,(double)sonic_mm,(int)count++);
+			//tft_clear();
+			tft_prints(0,0,"Dur:%d\nmm:%d\nCnt:%d",sonic_dur,(int)sonic_mm,(int)count);
+			tft_prints(0,3,"rise:%d",(int)rise);
+			tft_prints(0,4,"fall:%d",(int)fall);
 			tft_update();
+			uart_tx_str(COM1,"Dur:%d\tmm:%d\tCnt:%d\n",sonic_dur,(int)sonic_mm,(int)count++); 
 		}	
 	}
 }
